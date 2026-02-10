9390c74f977510aab48bbb5c2832285da447fd15
Christian Sheen <csheen1725@gmail.com>
2026-02-10 15:56:00 -0500

    Custom hook to get \'Pizza of the day\'

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
-import Pizza from "./Pizza";
+import { StrictMode } from "react";
+import Order from "./Order";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 
 const App = () => {
   return (
-    <div>
-      <Pizza
-        name="Peperonni Pizza"
-        description="The Classic spicy cold cut pizza everybody loves"
-        image={"/public/pizzas/pepperoni.webp"}
-      />
-      <Pizza
-        name="Hawaiian"
-        description="Ham & Pineapple"
-        image={"/public/pizzas/hawaiian.webp"}
-      />
-      <Pizza
-        name="Meat Lovers"
-        description="Pepperoni + Ham + Sausage + Chicken"
-        image={"/public/pizzas/big_meat.webp"}
-      />
-    </div>
+    <StrictMode>
+      <div>
+        <h1 className="logo">Padre Gino's Pizza</h1>
+        <Order />
+        <PizzaOfTheDay />
+      </div>
+    </StrictMode>
   );
 };
 
