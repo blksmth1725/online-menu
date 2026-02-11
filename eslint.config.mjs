c9dc88bbe67e943e86edbb42b4730bf37283f054
Christian Sheen <csheen1725@gmail.com>
2026-02-11 11:03:10 -0500

    TanStack router added to project && /past-orders route created to view passed orders with implicit caching

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
 import reactPlugin from "eslint-plugin-react";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 /** @type {import('eslint').Linter.Config[]} */
 
@@ -15,8 +16,8 @@ export default [
       },
     },
   },
-  reactPlugin.config.flat["jsx-runtime"],
-
+  reactPlugin.configs.flat["jsx-runtime"],
+  ...pluginQuery.configs["flat/recommended"],
   {
     files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import { StrictMode, useState } from "react";
-import { CartContext } from "./contexts";
-import Header from "./Header";
-import Order from "./Order";
-import PizzaOfTheDay from "./PizzaOfTheDay";
+import { StrictMode } from "react";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
 
 const App = () => {
-  const cartHook = useState([]);
   return (
     <StrictMode>
-      <CartContext.Provider value={cartHook}>
-        <div>
-          <Header />
-          <Order />
-          <PizzaOfTheDay />
-        </div>
-      </CartContext.Provider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>
   );
 };
